cce931d41e7024d08dbe74dd46e4aa367b832b30
Arjan Topolovec <arjan@celtra.com>
2018-05-20 18:03:34 +0200

    Add opencensus stats to client example.

--- examples/client.go
+++ examples/client.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"context"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 
+	"encoding/hex"
 	"github.com/arjantop/pwned-passwords/pwnedpasswords"
+	"go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
 	"google.golang.org/grpc"
 )
 
 var (
-	serverAddr = flag.String("addr", "", "address and port of remote server")
+	serverAddr   = flag.String("addr", "", "address and port of remote server")
+	gcpProjectId = flag.String("gcpProjectId", "", "Google Cloud project id")
 )
 
 func main() {
@@ -27,10 +32,33 @@ func main() {
 
 	prefix := flag.Arg(0)
 
-	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	var exporter *stackdriver.Exporter
+	if *gcpProjectId != "" {
+		log.Println("Initializing stackdriver exporter ...")
+		var err error
+		exporter, err = stackdriver.NewExporter(stackdriver.Options{ProjectID: *gcpProjectId})
+		if err != nil {
+			log.Fatalf("Could not initialize stackdriver exporter: %s", err)
+		}
+
+		view.RegisterExporter(exporter)
+		trace.RegisterExporter(exporter)
+	}
+	defer func() {
+		if exporter != nil {
+			log.Println("Flushing stats ...")
+			exporter.Flush()
+		}
+	}()
+
+	// For demo purposes
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithStatsHandler(&ocgrpc.ClientHandler{}), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not dial: %s", err)
 	}
+	defer conn.Close()
 
 	client := pwnedpasswords.NewPwnedPasswordsClient(conn)
 	r, err := client.ListHashesForPrefix(context.Background(), &pwnedpasswords.ListRequest{
@@ -48,7 +76,8 @@ func main() {
 			break
 		}
 		if err != nil {
-			log.Fatalf("Receiving failed: %s", err)
+			log.Printf("Receiving failed: %s", err)
+			return
 		}
 		fmt.Printf(hex.EncodeToString(h.HashSuffix))
 		fmt.Println()
